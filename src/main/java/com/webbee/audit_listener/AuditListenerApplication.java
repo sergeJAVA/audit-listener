67a8c2d4cf1deb4669e2b0b1a42b84bdba7a695a
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-08-06 15:28:13 +0300

    first commit

---
+++ src/main/java/com/webbee/audit_listener/AuditListenerApplication.java
@@ -0,0 +1,13 @@
+package com.webbee.audit_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditListenerApplication.class, args);
+	}
+
+}
