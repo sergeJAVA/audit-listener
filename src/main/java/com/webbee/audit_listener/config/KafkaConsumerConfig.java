25e1ed67c7ba4939bfcca8c8c7b1bba90613b564
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-08-08 19:14:40 +0300

    test: Добавил тест для проверки exactly-one вычитки сообщения

--- src/main/java/com/webbee/audit_listener/config/KafkaConsumerConfig.java
+++ src/main/java/com/webbee/audit_listener/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.webbee.audit_listener.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -14,10 +15,13 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${audit.kafka.bootstrap-servers}")
+    private String kafkaBootstrapServers;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "audit-log-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
